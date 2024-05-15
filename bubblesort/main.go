bafaabb102f97189581d702e52e870e0c7e83a6c
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2024-05-14 20:11:18 -0400

    Bubble Sort com Ponteiros

--- bubblesort/main.go
+++ bubblesort/main.go
@@ -4,24 +4,23 @@ import (
 	"fmt"
 )
 
-func bubbleSort(arr []int) []int {
-	n := len(arr)
+func bubbleSort(arr *[]int) {
+	n := len(*arr)
 	swapped := true
 
 	for swapped {
 		swapped = false
 		for i := 1; i < n; i++ {
-			if arr[i-1] > arr[i] {
-				arr[i], arr[i-1] = arr[i-1], arr[i]
+			if (*arr)[i-1] > (*arr)[i] {
+				(*arr)[i], (*arr)[i-1] = (*arr)[i-1], (*arr)[i]
 				swapped = true
 			}
 		}
 	}
-	return arr
 }
 
 func main() {
 	arr := []int{10, 5, 3, 8, 4, 2}
-	arr = bubbleSort(arr)
+	bubbleSort(&arr)
 	fmt.Println(arr)
 }
