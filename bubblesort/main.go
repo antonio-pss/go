971c028a1916a212ca5fd6b6619d1413727d5c2a
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2024-05-17 18:04:34 -0400

    BubbleSort

--- bubblesort/main.go
+++ bubblesort/main.go
@@ -4,7 +4,17 @@ import (
 	"fmt"
 )
 
-func bubbleSort(arr *[]int) {
+func bubbleSort1(arr *[]int) {
+	for i := 0; i < len(*arr)-1; i++ {
+		for j := 0; j < len(*arr)-i-1; j++ {
+			if (*arr)[j] < (*arr)[j+1] {
+				(*arr)[j], (*arr)[j+1] = (*arr)[j+1], (*arr)[j]
+			}
+		}
+	}
+}
+
+func bubbleSort2(arr *[]int) {
 	n := len(*arr)
 	swapped := true
 
@@ -21,6 +31,6 @@ func bubbleSort(arr *[]int) {
 
 func main() {
 	arr := []int{10, 5, 3, 8, 4, 2}
-	bubbleSort(&arr)
+	bubbleSort1(&arr)
 	fmt.Println(arr)
 }
